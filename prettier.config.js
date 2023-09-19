3509233e2b353bdb4edb6573338eb0948fce85a0
Mike Mitterer <office@mikemitterer.at>
2023-09-19 12:53:29 +0200

    Auf Module umgestellt

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    trailingComma: 'none',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-    printWidth: 120,
-    arrowParens: 'always',
-    bracketSpacing: true
-}
